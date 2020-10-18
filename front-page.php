65aef91db5778940a25c2a0e2b8be36a2e6e0acf
unknown <kamok@bk.ru>
2020-10-18 21:27:52 +0300

    sidebar + widget + posts

--- front-page.php
+++ front-page.php
@@ -89,7 +89,7 @@
                     <li class="post">
                         <?php the_category(); ?>
                         <a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
-                            <h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...' . '-->' . ';))'); ?></h4>
+                            <h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...' ); ?></h4>
                         </a>
                     </li>
                     <!--  -->
@@ -113,6 +113,7 @@
 </main>
 
 <div class="container">
+
     <ul class="article-list">
         <!--  -->
         <?php
@@ -145,4 +146,164 @@
             wp_reset_postdata(); // Сбрасываем $post
         ?>
     </ul>
+
+    <div class="main-grid">
+        <ul class="article-grid">
+            <?php		
+                global $post;
+                // запрос в базу
+                $query = new WP_Query( [
+                    'posts_per_page' => 7, 
+                ] );
+
+                if ( $query -> have_posts() ) {
+                    // формируем счетчик постов 
+                    $cnt = 0;
+                    while ( $query->have_posts() ) {
+                        
+                        $query->the_post();
+                        $cnt++;
+                        
+                        switch ($cnt) {
+                            case '1':
+            ?> 
+                                <li class="article-grid-item article-grid-item-1">
+                                    <a href="<?php the_permalink() ?>" class="article-grid-permalink">
+                                    <img class="article-grid-thumb" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="" srcset="">
+                                        <span class="category-name">
+                                            <?php 
+                                                $category = get_the_category();
+                                                echo $category[0]->name;
+                                            ?>
+                                        </span>
+                                        <h4 class="article-grid-title">
+                                            <?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?>
+                                        </h4>
+                                        <div class="article-grid-excerpt">
+                                            <?php echo mb_strimwidth(get_the_excerpt(), 0, 150, '...'); ?>
+                                        </div>
+                                        <div class="article-grid-info">
+                                            <div class="author">
+                                                <?php $author_id = get_the_author_meta('ID'); ?>
+                                                <img src="<?php echo get_avatar_url($author_id); ?>" alt="author-avatar" class="author-avatar">
+                                                <span class="author-name">
+                                                    <strong><?php the_author(); ?>:</strong>
+                                                    <?php the_author_meta('description'); ?>
+                                                </span>
+                                            </div>
+                                            <div class="comments">
+                                                <img src="<?php echo get_template_directory_uri() . './assets/images/comment.svg' ?>" alt="comment-icon" class="comments-icon">
+                                                <span class="comments-counter"><?php comments_number('0', '1', '%'); ?></span>
+                                            </div>
+                                        </div>
+                                        <!-- <img src="<?php echo  get_the_post_thumbnail_url() ?>" alt="article-image" class=""> -->
+                                    </a>
+                                </li>
+            <?php
+                                break;
+                                    
+                                case '2':
+                                    
+            ?> 
+                                    <li class="article-grid-item article-grid-item-2">
+                                        <img class="article-grid-thumb" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="background">
+                                        <a href="<?php the_permalink() ?>" class="article-grid-permalink">
+                                            <span class="tag"> 
+                                                <?php 
+                                                    $posttags = get_the_tags();
+                                                    if($posttags) {
+                                                        echo $posttags[0]->name . ' ';
+                                                    }
+                                                ?>
+                                            </span>
+                                            <span class="category-name">
+                                                <?php 
+                                                    $category = get_the_category();
+                                                    echo $category[0]->name;
+                                                ?>
+                                            </span>
+                                            <h4 class="article-grid-title">
+                                                <?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?>
+                                            </h4>
+                                            <!-- <p class="article-grid-excerpt">
+                                                <?php the_excerpt(); ?>
+                                            </p> -->
+                                            <div class="article-grid-info">
+                                                <div class="author">
+                                                    <?php $author_id = get_the_author_meta('ID'); ?>
+                                                    <img src="<?php echo get_avatar_url($author_id); ?>" alt="author-avatar" class="author-avatar">
+                                                    <div class="author-info">
+                                                        <span class="author-name">
+                                                            <strong><?php the_author(); ?></strong>
+                                                        </span>
+                                                        <span class="date"><?php the_time('j F'); ?></span>
+                                                        <!-- </div> -->
+                                                        <div class="comments">
+                                                            <img src="<?php echo get_template_directory_uri() . './assets/images/comment-white.svg' ?>" alt="comment-icon" class="comments-icon">
+                                                            <span class="comments-counter"><?php comments_number('0', '1', '%'); ?></span>
+                                                        </div>
+                                                        <div class="likes">
+                                                            <img src="<?php echo get_template_directory_uri() . './assets/images/count-heart.svg' ?>" alt="likes-icon" class="likes-icon">
+                                                            <span class="comments-counter"><?php comments_number('0', '1', '%'); ?></span>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /.author-info -->
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </li>
+            <?php
+
+                                    break;
+
+                                    case '3':
+            ?> 
+                                        <li class="article-grid-item article-grid-item-3">
+                                            <a href="<?php the_permalink() ?>" class="article-grid-permalink">
+                                                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="article-image" class="article-thumb">
+                                                <h4 class="article-grid-title">
+                                                    <?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?>
+                                                </h4>
+                                            </a>
+                                        </li>
+            <?php
+                                        break;
+                            
+                                    default: // выводим остальные посты
+            ?>
+                                        <li class="article-grid-item article-grid-item-default">
+                                            <a href="<?php the_permalink() ?>" class="article-grid-permalink">
+                                                <h4 class="article-grid-title">
+                                                    <?php echo mb_strimwidth(get_the_title(), 0, 20, '...'); ?>
+                                                </h4>
+                                                <div class="article-grid-excerpt">
+                                                    <?php echo mb_strimwidth(get_the_excerpt( ), 0, 90, '...'); ?>
+                                                </div>
+                                                <span class="article-grid-data"><?php the_time('j F'); ?></span>
+                                            </a>
+                                        </li>
+            <?php
+                                        break;
+                        }
+            ?>
+                <!-- Вывода постов, функции цикла: the_title() и т.д. -->
+            <?php 
+                    }
+                } else {
+                    // Постов не найдено
+                }
+
+                wp_reset_postdata(); // Сбрасываем $post
+            ?>
+        </ul>
+        <!-- /.article-grid -->
+
+        <!-- //Подключаем сайдбар -->
+        <?php get_sidebar('front-page'); ?>
+        
+    </div>
+    
 </div>
+
+<?php get_footer(); ?>
+
--- functions.php
+++ functions.php
@@ -35,12 +35,159 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
 
         // регистрация меню сайта
         register_nav_menus( [
-            'header_menu' => 'Меню в шапке',
-            'footer_menu' => 'Меню в подвале'
+            'header_menu' => 'header',
+            'footer_menu' => 'footer'
         ] );
     };
 endif;
 add_action( 'after_setup_theme', 'universal_theme_setup' );
+
+/**
+ * Подключение сайд-бара.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function universal_theme_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar main page', 'universal-theme' ),
+			'id'            => 'main-sidebar',
+			'description'   => esc_html__( 'Add widgets here.', 'universal-theme' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'universal_theme_widgets_init' );
+
+
+
+/**
+ * Добавление нового виджета Downloader_Widget.
+ */
+class Downloader_Widget extends WP_Widget {
+
+	// Регистрация виджета используя основной класс
+	function __construct() {
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'downloader_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: downloader_widget
+			'Полезные файлы',
+			array( 'description' => 'Файлы для скачивания', 'classname' => 'widget-downloader', )
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action('wp_enqueue_scripts', array( $this, 'add_downloader_widget_scripts' ));
+			add_action('wp_head', array( $this, 'add_downloader_widget_style' ) );
+		}
+	}
+
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget( $args, $instance ) {
+        $title = $instance['title'];
+        $description = $instance['description'];
+        $link = $instance['link'];
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+        }
+        if ( ! empty( $description ) ) {
+			echo '<p class="widget-description">' . $description . '</p>';
+        }
+        if ( ! empty( $link ) ) {
+			echo '<a target="_blank" class="widget-link" href="' . $link . '">
+					<img class="widget-link-icon" src="' . get_template_directory_uri(). '/assets/images/download.svg" >
+					Скачать</a>';
+		}
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form( $instance ) {
+		$title = @ $instance['title'] ?: 'Полезные файлы';
+        $description = @ $instance['description'] ?: 'Описание';
+        $link = @ $instance['link'] ?: 'http://yandex.ru/';
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заголовок:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+        <p>
+			<label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Описание:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>">
+		</p>
+        <p>
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Ссылка на файл:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        $instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
+        $instance['link'] = ( ! empty( $new_instance['link'] ) ) ? strip_tags( $new_instance['link'] ) : '';
+
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_downloader_widget_scripts() {
+		// фильтр чтобы можно было отключить скрипты
+		if( ! apply_filters( 'show_downloader_widget_script', true, $this->id_base ) )
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('downloader_widget_script', $theme_url .'/downloader_widget_script.js' );
+	}
+
+	// стили виджета
+	function add_downloader_widget_style() {
+		// фильтр чтобы можно было отключить стили
+		if( ! apply_filters( 'show_downloader_widget_style', true, $this->id_base ) )
+			return;
+		?>
+		<style type="text/css">
+			.downloader_widget a{ display:inline; }
+		</style>
+		<?php
+	}
+
+} 
+// конец класса Downloader_Widget
+
+// регистрация Downloader_Widget в WordPress
+function register_downloader_widget() {
+	register_widget( 'Downloader_Widget' );
+}
+add_action( 'widgets_init', 'register_downloader_widget' );
+
 
 
 ## отключаем создание миниатюр файлов для указанных размеров
