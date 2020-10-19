fe427351870c36a9fa4b3e58ed2c8a9c33cf8e63
unknown <kamok@bk.ru>
2020-10-19 12:57:37 +0300

    tags-cloud + social widget

--- functions.php
+++ functions.php
@@ -15,6 +15,20 @@ function enqueue_universal_style() {
 add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
 
 
+// Добавляем Теги
+function edit_widget_tag_cloud_args($args)
+{
+	$args['unit'] = 'px';
+	$args['smallest'] = '14';
+	$args['largest'] = '14';
+	$args['number'] = '13';
+
+	return $args;
+}
+add_filter('widget_tag_cloud_args', 'edit_widget_tag_cloud_args');
+
+
+
 // добавляем расширеные возможности
 if ( ! function_exists( 'universal_theme_setup' ) ) :
     // добавляем тег title 
@@ -187,6 +201,147 @@ function register_downloader_widget() {
 	register_widget( 'Downloader_Widget' );
 }
 add_action( 'widgets_init', 'register_downloader_widget' );
+
+
+
+///////////////////////////////////////////////////////////
+
+
+class Social_Widget extends WP_Widget {
+
+	// Регистрация виджета используя основной класс
+	function __construct() {
+		// вызов конструктора выглядит так:
+		// __construct( $id_base, $name, $widget_options = array(), $control_options = array() )
+		parent::__construct(
+			'social_widget', // ID виджета, если не указать (оставить ''), то ID будет равен названию класса в нижнем регистре: downloader_widget
+			'Социальные сети',
+			array( 'description' => 'Социальные сети', 'classname' => 'social_widget', )
+		);
+
+		// скрипты/стили виджета, только если он активен
+		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
+			add_action('wp_enqueue_scripts', array( $this, 'add_social_widget_scripts' ));
+			add_action('wp_head', array( $this, 'add_social_widget_style' ) );
+		}
+	}
+
+	/**
+	 * Вывод виджета во Фронт-энде
+	 *
+	 * @param array $args     аргументы виджета.
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function widget( $args, $instance ) {
+        $title = $instance['title'];
+        // $description = $instance['description'];
+		$link_fb = $instance['link'];
+		$link_twt = $instance['link'];
+		$link_ytb = $instance['link'];
+
+
+		echo $args['before_widget'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+        }
+        // if ( ! empty( $description ) ) {
+		// 	echo '<p class="widget-description">' . $description . '</p>';
+        // }
+        if ( ! empty( $link_fb ) ) {
+			echo '<a target="_blank" class="widget-link" href="' . $link_fb . '">
+					<img class="fsb-link-icon" src="' . get_template_directory_uri(). '/assets/images/fsb.png" ></a>';
+		}
+		if ( ! empty( $link_twt ) ) {
+			echo '<a target="_blank" class="widget-link" href="' . $link_twt . '">
+					<img class="fsb-link-icon" src="' . get_template_directory_uri(). '/assets/images/twt.png" ></a>';
+		}
+		if ( ! empty( $link_ytb ) ) {
+			echo '<a target="_blank" class="widget-link" href="' . $link_ytb . '">
+					<img class="fsb-link-icon" src="' . get_template_directory_uri(). '/assets/images/ytb.png" ></a>';
+		}
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Админ-часть виджета
+	 *
+	 * @param array $instance сохраненные данные из настроек
+	 */
+	function form( $instance ) {
+		$title = @ $instance['title'] ?: 'Полезные файлы';
+        // $description = @ $instance['description'] ?: 'Описание';
+		$link_fb = @ $instance['link'] ?: 'http://fasebook.com/';
+		$link_fb = @ $instance['link'] ?: 'http://twitter.com/';
+		$link_fb = @ $instance['link'] ?: 'http://youtube.com/';
+		?>
+		<p>
+			<label  for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Заголовок:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+        <!-- <p>
+			<label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php _e( 'Описание:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo esc_attr( $description ); ?>">
+		</p> -->
+        <p>
+			<label for="<?php echo $this->get_field_id( 'link' ); ?>"><?php _e( 'Ссылка на файл:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" type="text" value="<?php echo esc_attr( $link_fb ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Сохранение настроек виджета. Здесь данные должны быть очищены и возвращены для сохранения их в базу данных.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance новые настройки
+	 * @param array $old_instance предыдущие настройки
+	 *
+	 * @return array данные которые будут сохранены
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        // $instance['description'] = ( ! empty( $new_instance['description'] ) ) ? strip_tags( $new_instance['description'] ) : '';
+        $instance['link'] = ( ! empty( $new_instance['link'] ) ) ? strip_tags( $new_instance['link'] ) : '';
+
+		return $instance;
+	}
+
+	// скрипт виджета
+	function add_social_widget_scripts() {
+		// фильтр чтобы можно было отключить скрипты
+		if( ! apply_filters( 'show_social_widget_script', true, $this->id_base ) )
+			return;
+
+		$theme_url = get_stylesheet_directory_uri();
+
+		wp_enqueue_script('social_widget_script', $theme_url .'/social_widget_script.js' );
+	}
+
+	// стили виджета
+	function add_social_widget_style() {
+		// фильтр чтобы можно было отключить стили
+		if( ! apply_filters( 'show_social_widget_style', true, $this->id_base ) )
+			return;
+		?>
+		<style type="text/css">
+			.social_widget a{ display:inline; }
+		</style>
+		<?php
+	}
+
+} 
+// конец класса Downloader_Widget
+
+// регистрация Downloader_Widget в WordPress
+function register_social_widget() {
+	register_widget( 'Social_Widget' );
+}
+add_action( 'widgets_init', 'register_social_widget' );
+
+
+////////////////////////////////////////////////////////////
 
 
 
